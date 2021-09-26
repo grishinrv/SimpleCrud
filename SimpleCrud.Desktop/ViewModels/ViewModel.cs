6c845c711a7bb2f03bdf8847f3a4c383ea5a0dbf
rgrishin <roman1988grishin@gmail.com>
2021-09-26 15:31:56 +0300

    JetBrains Annotations, base ViewModel

---
+++ SimpleCrud.Desktop/ViewModels/ViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SimpleCrud.Desktop.ViewModels
+{
+    public abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void OnSet<T>(ref T field, T newVal, Action<T, T> onChangedAction = null, [CallerMemberName] string propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, newVal))
+            {
+                var oldVal = field;
+                field = newVal;
+                OnPropertyChanged(propertyName);
+                onChangedAction?.Invoke(oldVal, newVal);
+            }
+        }
+    }
+}
