2d25d57d29bb45e977433c6298d4a15507e75587
rgrishin <roman1988grishin@gmail.com>
2021-09-30 20:24:42 +0300

    fix visual

--- SimpleCrud.Desktop/MainWindow.xaml.cs
+++ SimpleCrud.Desktop/MainWindow.xaml.cs
@@ -9,15 +9,11 @@ namespace SimpleCrud.Desktop
             InitializeComponent();
         }
 
-        private void HamburgerMenu_OnItemInvoked(object sender, HamburgerMenuItemInvokedEventArgs e)
-        {
-            this.HamburgerMenu.Content = e.InvokedItem;
 
-            if (!e.IsItemOptions && this.HamburgerMenu.IsPaneOpen)
-            {
-                // You can close the menu if an item was selected
-                // this.HamburgerMenu.SetCurrentValue(HamburgerMenuControl.IsPaneOpenProperty, false);
-            }
+        private void HamburgerMenuControl_OnItemClick(object sender, ItemClickEventArgs args)
+        {
+            this.HamburgerMenuControl.Content = args.ClickedItem;
+            this.HamburgerMenuControl.IsPaneOpen = false;
         }
     }
 }
