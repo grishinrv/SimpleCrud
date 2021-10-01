7a4241392ba4c073d35c75007492b28f9926d613
rgrishin <roman1988grishin@gmail.com>
2021-10-01 17:07:16 +0300

    ActivityControl template (in progress)

---
+++ SimpleCrud.Controls/ActivityControl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace SimpleCrud.Controls
+{
+    /// <summary>
+    /// A form (tab) template, with support of async operations (ui-notifying) and dialogs.
+    /// </summary>
+    public class ActivityControl : ContentControl
+    {
+    }
+}
