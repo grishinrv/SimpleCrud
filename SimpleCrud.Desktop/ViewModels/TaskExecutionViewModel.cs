0b380462612973c16066170194ff17ad4ba3ab72
rgrishin <roman1988grishin@gmail.com>
2021-10-01 13:49:14 +0300

    Dispose old task watcher

--- SimpleCrud.Desktop/ViewModels/TaskExecutionViewModel.cs
+++ SimpleCrud.Desktop/ViewModels/TaskExecutionViewModel.cs
@@ -3,6 +3,6 @@
     public class TaskExecutionViewModel : ViewModel
     {
         private ITaskWatcher _currentTask;
-        public ITaskWatcher CurrentTask { get => _currentTask; set => OnSet(ref _currentTask, value); }
+        public ITaskWatcher CurrentTask { get => _currentTask; set => OnSet(ref _currentTask, value, (o, _) => o?.Dispose()); }
     }
 }
