d8f0785afd0d2553bd1878ec685833d6289cdaef
rgrishin <roman1988grishin@gmail.com>
2021-10-01 14:31:15 +0300

    Progress

--- SimpleCrud.Desktop/ViewModels/CompaniesViewModel.cs
+++ SimpleCrud.Desktop/ViewModels/CompaniesViewModel.cs
@@ -1,5 +1,8 @@
-﻿using SimpleCrud.Storage.Models;
+﻿using System;
+using SimpleCrud.Storage.Models;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using SimpleCrud.MVVM.Commands.Parameters;
 using SimpleCrud.MVVM.ViewModels;
 
 namespace SimpleCrud.Desktop.ViewModels
@@ -7,11 +10,23 @@ namespace SimpleCrud.Desktop.ViewModels
 
     public class CompaniesViewModel : TaskExecutionViewModel
     {
-        public CompaniesViewModel()
+        public CompaniesViewModel() : base()
         {
             Rows = new ObservableCollection<Company>();
+            ResultParam = new AsyncFunctionContainer { Job = GetResultAsync };
         }
 
         public ObservableCollection<Company> Rows { get; }
+
+        private int _result;
+        public int Result { get => _result; set => OnSet(ref _result, value); }
+
+        public AsyncFunctionContainer ResultParam { get; }
+
+        private async Task GetResultAsync()
+        {
+            await Task.Delay(4000);
+            Result = new Random().Next(1, 999);
+        }
     }
 }
--- SimpleCrud.MVVM/ViewModels/TaskExecutionViewModel.cs
+++ SimpleCrud.MVVM/ViewModels/TaskExecutionViewModel.cs
@@ -1,7 +1,15 @@
-﻿namespace SimpleCrud.MVVM.ViewModels
+﻿using System.Runtime.CompilerServices;
+using SimpleCrud.MVVM.Commands;
+
+namespace SimpleCrud.MVVM.ViewModels
 {
-    public class TaskExecutionViewModel : ViewModel
+    public abstract class TaskExecutionViewModel : ViewModel
     {
+        protected TaskExecutionViewModel()
+        {
+            AsyncCommand = new RunLongTaskCommand(this);
+        }
+        public RunLongTaskCommand AsyncCommand { get; }
         private ITaskWatcher _currentTask;
         public ITaskWatcher CurrentTask { get => _currentTask; set => OnSet(ref _currentTask, value, (o, _) => o?.Dispose()); }
     }
