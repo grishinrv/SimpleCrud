04f4244f759b415d00883eb888eb60bd425a3e60
rgrishin <roman1988grishin@gmail.com>
2021-10-03 17:37:35 +0300

    Add dialog func

--- SimpleCrud.Controls/ActivityControl.cs
+++ SimpleCrud.Controls/ActivityControl.cs
@@ -11,8 +11,15 @@ namespace SimpleCrud.Controls
     /// <summary>
     /// A form (tab) template, with support of async operations (ui-notifying) and dialogs.
     /// </summary>
+    [TemplatePart(Name = PART_ActiveDialogContainer, Type = typeof(Grid))]
+    [TemplatePart(Name = PART_InactiveDialogsContainer, Type = typeof(Grid))]
     public sealed class ActivityControl : ContentControl
     {
+        #region Constants
+        private const string PART_ActiveDialogContainer = "PART_ActiveDialogContainer";
+        private const string PART_InactiveDialogsContainer = "PART_InactiveDialogsContainer";
+        #endregion
+
         #region Dependency properties declarations
 
         public static readonly DependencyProperty JobDataContextProperty =
@@ -25,7 +32,38 @@ namespace SimpleCrud.Controls
         public override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
-            // Parts initialization
+            this.ActiveDialogContainer = this.GetTemplateChild(PART_ActiveDialogContainer) as Grid;
+            this.InActiveDialogsContainer = this.GetTemplateChild(PART_InactiveDialogsContainer) as Grid;
+        }
+
+        public Grid ActiveDialogContainer;
+        public Grid InActiveDialogsContainer;
+
+        private static void AddDialog(ActivityControl activity, ProgressControl progress)
+        {
+            if (activity.ActiveDialogContainer is null)
+            {
+                throw new InvalidOperationException("Active progress container could not be found.");
+            }
+
+            if (activity.InActiveDialogsContainer is null)
+            {
+                throw new InvalidOperationException("Inactive progress container could not be found.");
+            }
+
+            //activity.StoreFocus();
+
+            // if there's already an active progress, move to the background
+            var activeDialog = activity.ActiveDialogContainer.Children.OfType<ProgressControl>().SingleOrDefault();
+            if (activeDialog != null)
+            {
+                activity.ActiveDialogContainer.Children.Remove(activeDialog);
+                activity.InActiveDialogsContainer.Children.Add(activeDialog);
+            }
+
+            activity.ActiveDialogContainer.Children.Add(progress); 
+
+            //activity.SetValue(MetroWindow.IsAnyDialogOpenPropertyKey, BooleanBoxes.TrueBox);
         }
 
         #region  Dependency properties accessors
