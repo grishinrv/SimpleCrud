66329dd5894c91e04c59b5377101fd565a4ed2a5
rgrishin <roman1988grishin@gmail.com>
2021-10-12 11:11:41 +0300

    refactoring ProgressView (UserControl) -> ProgressDialog (Control)

---
+++ SimpleCrud.Controls/ProgressDialog.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SimpleCrud.Controls
+{
+    public sealed class ProgressDialog : Control
+    {
+        static ProgressDialog()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ProgressDialog), new FrameworkPropertyMetadata(typeof(ProgressDialog)));
+        }
+    }
+}
