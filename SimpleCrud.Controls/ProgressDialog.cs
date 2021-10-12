a3dbbeed91a69f6f10c4e730a9560be386556e42
rgrishin <roman1988grishin@gmail.com>
2021-10-12 11:26:51 +0300

    Template part

--- SimpleCrud.Controls/ProgressDialog.cs
+++ SimpleCrud.Controls/ProgressDialog.cs
@@ -1,13 +1,34 @@
-﻿using System.Windows;
+﻿using System.CodeDom;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace SimpleCrud.Controls
 {
+    [TemplatePart(Name = PART_ProgressBox, Type = typeof(ProgressControl))]
     public sealed class ProgressDialog : Control
     {
+        private const string PART_ProgressBox = "PART_ProgressBox";
+        
+        public static readonly DependencyProperty PropertyTypeProperty = DependencyProperty.Register(
+            nameof(IsDialogShown), typeof(bool), typeof(ProgressDialog), new PropertyMetadata(default(bool)));
+
         static ProgressDialog()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(ProgressDialog), new FrameworkPropertyMetadata(typeof(ProgressDialog)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            _progress = GetTemplateChild(PART_ProgressBox) as ProgressControl;
+        }
+
+        private ProgressControl _progress; 
+        
+        public bool IsDialogShown
+        {
+            get { return (bool)GetValue(PropertyTypeProperty); }
+            set { SetValue(PropertyTypeProperty, value); }
         }
     }
 }
