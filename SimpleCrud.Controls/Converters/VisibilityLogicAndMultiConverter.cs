131d97d8748188fffa63ff2f2a215b7ead05a4ed
rgrishin <roman1988grishin@gmail.com>
2021-10-16 16:20:14 +0300

    Overaly visibility binding (multiple)

---
+++ SimpleCrud.Controls/Converters/VisibilityLogicAndMultiConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SimpleCrud.Controls.Converters
+{
+    public class VisibilityLogicAndMultiConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            foreach (var value in values)
+            {
+                Visibility visibility = (Visibility)value;
+                if (visibility != Visibility.Visible)
+                    return Visibility.Collapsed;
+            }
+
+            return Visibility.Visible;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) => null;
+    }
+}
