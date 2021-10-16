ec2309c8f6d8345b9c85d4663664a78adaac3562
rgrishin <roman1988grishin@gmail.com>
2021-10-16 04:09:56 +0300

    + IJobController

---
+++ SimpleCrud.Infrastructure.Job/Services/IJobController.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Input;
+
+namespace SimpleCrud.Infrastructure.Job.Services
+{
+    public interface IJobController
+    {
+        string JobSourceName { get; }
+        ICommand ProcessErrorCommand { get; }
+        ICommand RunJobCommand { get; }
+        JobCompletionStatus LastJobStatus { get; }
+        void BeginExecute(JobData data);
+    }
+}
